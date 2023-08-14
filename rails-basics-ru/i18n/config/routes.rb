94276f8e0a459c20010012914e06654128113379
@hexlet/cli <support@hexlet.io>
2023-08-14 22:06:09 +0300

    submit rails-basics-ru/i18n

---
+++ rails-basics-ru/i18n/config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # BEGIN
+  scope '/(:locale)', locale: /en|ru/ do
+    root 'home#index'
+
+    resources :posts do
+      scope module: :posts do
+        resources :comments, except: %i[index show new]
+      end
+    end
+  end
+  # END
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
